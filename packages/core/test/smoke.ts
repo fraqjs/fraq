7040a338839b4dcc52a66f3171f4216453dbbd4d
Wei FeiYang <wesley.f.young@outlook.com>
2026-06-02 22:02:30 +0800

    [core] Use simple log handler for smoke test

--- packages/core/test/smoke.ts
+++ packages/core/test/smoke.ts
@@ -1,6 +1,10 @@
+import { createSimpleLogHandler } from '@fraqjs/mock';
+
 import { Context, definePlugin, msg, param, seg } from '../src';
 
-const ctx = Context.fromUrl('http://localhost:30001/');
+const ctx = Context.fromUrl('http://localhost:30001/', {
+  logHandler: createSimpleLogHandler(),
+});
 
 const EchoPlugin = definePlugin({
   name: 'echo',
