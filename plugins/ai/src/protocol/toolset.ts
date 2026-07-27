df6680cd654739d434bd9bd1ddbdf00fb223ac01
Wei FeiYang <wesley.f.young@outlook.com>
2026-07-27 14:37:49 +0800

    [plugin/ai] Shrink bundle size, bump version to 0.6.2

--- plugins/ai/src/protocol/toolset.ts
+++ plugins/ai/src/protocol/toolset.ts
@@ -1,12 +1,10 @@
-import type { Context } from '@fraqjs/fraq';
+import type { Context, milky } from '@fraqjs/fraq';
 import type { Tool } from 'ai';
 import { z } from 'zod';
 
 import * as mz from './types-zod';
 
-export type ToolApiEndpoint = keyof typeof mz.zodApiEndpoints;
-
-export function milkyToolset<T extends ToolApiEndpoint>(ctx: Context, endpoints: T[]): Record<T, Tool> {
+export function milkyToolset<T extends keyof milky.ApiEndpoints>(ctx: Context, endpoints: T[]): Record<T, Tool> {
   const tools: Record<string, Tool> = {};
   for (const endpoint of endpoints) {
     const endpointDesc = mz.zodApiEndpoints[endpoint];
